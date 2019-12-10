a616f17fae21a99314e67f43042088eb9657ea33
Matt Wright <mttwright@gmail.com>
2019-12-10 02:42:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,8 +23,9 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      
+    SQL
   end
   
   def save
