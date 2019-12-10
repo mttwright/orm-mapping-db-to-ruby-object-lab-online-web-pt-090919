e15dbc3afce33e22c5b1527a9388f4c916b912ef
Matt Wright <mttwright@gmail.com>
2019-12-10 04:53:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,4 +105,13 @@ class Student
       self.new_from_db(student)
     end.first
   end
+  
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql, grade)
+  end
 end
